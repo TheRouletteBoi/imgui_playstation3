042e1ac82c6915348a09a81422be0670ec8e746e
TheRouletteBoi <therouletteboi@outlook.com>
2022-04-27 14:57:02 -0700

    move all project files to 'examples' directory

---
+++ examples/example_playstation3_gcm_prx_hook/main.cpp
@@ -0,0 +1,11 @@
+#include <cellstatus.h>
+#include <sys/prx.h>
+
+SYS_MODULE_INFO( playstation3_gcm_prx_hook, 0, 1, 1);
+SYS_MODULE_START( _example_playstation3_gcm_prx_hook_prx_entry );
+
+extern "C" int _example_playstation3_gcm_prx_hook_prx_entry(void)
+{
+
+    return 0;
+}
